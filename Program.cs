a812d4b20661fc851ec0b79fbd345e3f1b4bb90a
Thanaporn002 <thanaporn.boon@kmutt.ac.th>
2022-11-24 12:24:37 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EvCharge
+{
+    class Program
+    {
+        static Menu menu;
+
+        static void Main(string[] args) {
+            ListUsersWhenLogin();
+
+            ShowMenu();
+        }
+
+        static void ListUsersWhenLogin() {
+            Program.menu = new Menu();
+        }
+
+        static void ShowMenu() {
+            Program.menu.ShowMenu();
+        }
+
+    }
+}
